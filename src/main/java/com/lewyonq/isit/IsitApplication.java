46d9209f2052b10733d8e1d2a6ed3ec9de2fb21f
lewyonq <jakub.lewandowski98@gmail.com>
2023-03-09 18:22:42 +0100

    first commit

---
+++ src/main/java/com/lewyonq/isit/IsitApplication.java
@@ -0,0 +1,13 @@
+package com.lewyonq.isit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsitApplication.class, args);
+	}
+
+}
